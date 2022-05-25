c6e12d07a0507e9b1aeeb88af1dff7cb5d2b6889
Josue-dev <52864843+Josue-dev@users.noreply.github.com>
2022-05-24 22:21:46 -0500

    enviando los primeros mensajes y recibiendo via socket

--- bot/bot.ts
+++ bot/bot.ts
@@ -8,11 +8,20 @@ export  const botTelegram =()=>{
     const bot = new TelegramBot(token, {polling:true});
 
 
+    bot.onText(/^\/start/,(msg)=>{
+        const chatId = msg.chat.id;
+        const nameUser = msg.from?.first_name;
+        const alias = msg.from?.username;
+        
+        bot.sendMessage(chatId, "Bienvenido  \n" + nameUser+"\n "+alias+"\n COMANDOS :\n 211 saliendo \n 211 ingresando ");
+        return;
+    });
 
 
-    bot.on('message',(msg)=>{
-        // const idNumeroTelgram = msg.from?.id;
-        // const nombre = msg.from?.first_name;
+    bot.onText(/^\/mensaje/,(msg)=>{
+        const chatId = msg.chat.id;
+        const idNumeroTelgram = msg.from?.id;
+        const nombre = msg.from?.first_name;
         const de = msg.from?.username;
 
         const cuerpo = msg.text;
@@ -25,5 +34,73 @@ export  const botTelegram =()=>{
         const server = ServerSocket.instance;
         server.io.emit('mensaje-nuevo',payload);
 
+        bot.sendMessage(chatId, "Su reporte fue exitoso!!");
+
     })
+
+
+
+    
+ bot.onText(/^\/reporte/,function(msg){
+    var chatId=msg.chat.id;
+
+    var botones = {
+        reply_markup:{
+            inline_keyboard:[
+                [
+                    {text:"saliendo",callback_data:'boton1'},
+                    {text:"ingresando",callback_data:'boton2'}
+                ]
+            ]
+        }
+    };
+
+    bot.sendMessage(chatId,"Marcar Reporte",botones);
+
+   
+    bot.on('callback_query',function onCallbackQuery(accionboton){
+        const data = accionboton.data;
+       
+        
+
+        // let fecha = accionboton.message?.date;
+        // const dt = new Date(fecha*1000); 
+        // var nuevaFecha = dt.getHours() + ':' + dt.getMinutes() + ':' + dt.getSeconds() + ' -- ' + dt;
+        if(data=='boton1'){
+        const cuerpo='saliendo';
+        const myId = accionboton.from.id;
+        const nombre = accionboton.from.first_name;
+        const de = accionboton.from.username;
+        const payload={
+            de,
+            cuerpo
+        }
+        console.log(payload);
+        const server = ServerSocket.instance;
+        server.io.emit('mensaje-nuevo',payload);
+
+       
+
+            bot.answerCallbackQuery(accionboton.id, {text: 'Reporte de salida agregado correctamente', show_alert: true});
+        }
+        if(data=='boton2'){
+            const cuerpo='ingresando';
+            const myId = accionboton.from.id;
+            const nombre = accionboton.from.first_name;
+            const de = accionboton.from.username;
+            const payload={
+                de,
+                cuerpo
+            }
+            console.log(payload);
+            const server = ServerSocket.instance;
+            server.io.emit('mensaje-nuevo',payload);
+    
+           
+    
+                bot.answerCallbackQuery(accionboton.id, {text: 'Reporte de ingreso agregado correctamente', show_alert: true});
+        }
+       
+    })
+})
 }
--- dist/bot/bot.js
+++ dist/bot/bot.js
@@ -10,11 +10,20 @@ const server_1 = __importDefault(require("../server/server"));
 const botTelegram = () => {
     const token = '5345732567:AAHA5Ax2aCmT8jO4bqcMwiTxOmKmooxc6LA';
     const bot = new node_telegram_bot_api_1.default(token, { polling: true });
-    bot.on('message', (msg) => {
-        var _a;
-        // const idNumeroTelgram = msg.from?.id;
-        // const nombre = msg.from?.first_name;
-        const de = (_a = msg.from) === null || _a === void 0 ? void 0 : _a.username;
+    bot.onText(/^\/start/, (msg) => {
+        var _a, _b;
+        const chatId = msg.chat.id;
+        const nameUser = (_a = msg.from) === null || _a === void 0 ? void 0 : _a.first_name;
+        const alias = (_b = msg.from) === null || _b === void 0 ? void 0 : _b.username;
+        bot.sendMessage(chatId, "Bienvenido  \n" + nameUser + "\n " + alias + "\n COMANDOS :\n 211 saliendo \n 211 ingresando ");
+        return;
+    });
+    bot.onText(/^\/mensaje/, (msg) => {
+        var _a, _b, _c;
+        const chatId = msg.chat.id;
+        const idNumeroTelgram = (_a = msg.from) === null || _a === void 0 ? void 0 : _a.id;
+        const nombre = (_b = msg.from) === null || _b === void 0 ? void 0 : _b.first_name;
+        const de = (_c = msg.from) === null || _c === void 0 ? void 0 : _c.username;
         const cuerpo = msg.text;
         const payload = {
             de,
@@ -23,6 +32,55 @@ const botTelegram = () => {
         console.log(payload);
         const server = server_1.default.instance;
         server.io.emit('mensaje-nuevo', payload);
+        bot.sendMessage(chatId, "Su reporte fue exitoso!!");
+    });
+    bot.onText(/^\/reporte/, function (msg) {
+        var chatId = msg.chat.id;
+        var botones = {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "saliendo", callback_data: 'boton1' },
+                        { text: "ingresando", callback_data: 'boton2' }
+                    ]
+                ]
+            }
+        };
+        bot.sendMessage(chatId, "Marcar Reporte", botones);
+        bot.on('callback_query', function onCallbackQuery(accionboton) {
+            const data = accionboton.data;
+            // let fecha = accionboton.message?.date;
+            // const dt = new Date(fecha*1000); 
+            // var nuevaFecha = dt.getHours() + ':' + dt.getMinutes() + ':' + dt.getSeconds() + ' -- ' + dt;
+            if (data == 'boton1') {
+                const cuerpo = 'saliendo';
+                const myId = accionboton.from.id;
+                const nombre = accionboton.from.first_name;
+                const de = accionboton.from.username;
+                const payload = {
+                    de,
+                    cuerpo
+                };
+                console.log(payload);
+                const server = server_1.default.instance;
+                server.io.emit('mensaje-nuevo', payload);
+                bot.answerCallbackQuery(accionboton.id, { text: 'Reporte de salida agregado correctamente', show_alert: true });
+            }
+            if (data == 'boton2') {
+                const cuerpo = 'ingresando';
+                const myId = accionboton.from.id;
+                const nombre = accionboton.from.first_name;
+                const de = accionboton.from.username;
+                const payload = {
+                    de,
+                    cuerpo
+                };
+                console.log(payload);
+                const server = server_1.default.instance;
+                server.io.emit('mensaje-nuevo', payload);
+                bot.answerCallbackQuery(accionboton.id, { text: 'Reporte de ingreso agregado correctamente', show_alert: true });
+            }
+        });
     });
 };
 exports.botTelegram = botTelegram;
